14a4649febc9cd0e3a02927f1fdeee627b7cd659
xtomdex <sharonovde@gmail.com>
2026-02-13 15:48:46 +0100

    Added response error code enum in exception mapping

--- src/Infrastructure/CommandBus/CommandBusExceptionMapper.php
+++ src/Infrastructure/CommandBus/CommandBusExceptionMapper.php
@@ -7,6 +7,7 @@ namespace App\Infrastructure\CommandBus;
 use App\Domain\Contract\CommandBusExceptionMapperInterface;
 use App\Domain\DTO\CommandBusError;
 use App\Domain\DTO\RedirectPayload;
+use App\Domain\Enum\ResponseErrorCode;
 use App\Domain\Exception\ForbiddenException;
 use App\Domain\Exception\NotFoundException;
 use App\Domain\Exception\RedirectException;
@@ -20,17 +21,26 @@ final class CommandBusExceptionMapper implements CommandBusExceptionMapperInterf
             $e instanceof ValidationException => new CommandBusError(
                 statusCode: 400,
                 message: $e->getMessage() ?: 'Validation failed',
+                errorCode: ResponseErrorCode::Validation,
                 errors: $e->getErrors(),
             ),
-            $e instanceof NotFoundException => new CommandBusError(404, $e->getMessage() ?: 'Not found'),
-            $e instanceof ForbiddenException => new CommandBusError(403, $e->getMessage() ?: 'Forbidden'),
+            $e instanceof NotFoundException => new CommandBusError(
+                statusCode: 404,
+                message: $e->getMessage() ?: 'Not found',
+                errorCode: ResponseErrorCode::NotFound
+            ),
+            $e instanceof ForbiddenException => new CommandBusError(
+                statusCode: 403,
+                message: $e->getMessage() ?: 'Forbidden',
+                errorCode: ResponseErrorCode::Forbidden
+            ),
             //$e instanceof AuthenticationException => new CommandBusError(401, $e->getMessage() ?: 'Unauthorized'),
             $e instanceof RedirectException => new CommandBusError(
                 statusCode: $e->statusCode,
                 message: $e->getMessage(),
                 redirect: new RedirectPayload($e->route, $e->params, $e->statusCode),
             ),
-            default => new CommandBusError(500, 'Internal server error'),
+            default => new CommandBusError(500),
         };
     }
 }
